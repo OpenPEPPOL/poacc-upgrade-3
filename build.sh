660f93c7efafce8ec61d428238ed4aba117e0117
Erlend Klakegg Bergheim <erlend@klakegg.net>
2019-06-12 09:58:28 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -32,3 +32,5 @@ docker run --rm -i -v $PROJECT:/documents -v $PROJECT/target:/target difi/asciid
 
 # Fix ownership
 docker run --rm -i -v $PROJECT:/src alpine:3.6 chown -R $(id -g $USER).$(id -g $USER) /src/target
+
+find target -type d | sort
