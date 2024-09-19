611384f024e3c268721a96424b3b8b0aa4f23d2b
Jerry Dimitriou <jd@inteleproc.eu>
2024-09-19 17:34:01 +0300

    zip fix

--- build.sh
+++ build.sh
@@ -20,11 +20,16 @@ docker run --rm -i -v $PROJECT:/src anskaffelser/validator:2.1.0 build -x -t -n 
 for sch in $PROJECT/rules/sch/*.sch; do
     docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/schematron:/target klakegg/schematron prepare /src/rules/sch/$(basename $sch) /target/$(basename $sch)
 done
-rm -rf /src/PEPPOLBIS-Upgrade-Schematron.zip
-zip -r target/site/files/PEPPOLBIS-Upgrade-Schematron.zip target/schematron
+
+# Fix ownership
+docker run --rm -i -v $PROJECT:/src alpine:3.6 chown -R $(id -g $USER).$(id -g $USER) /src/target
+
+rm -rf $PROJECT/target/site/files/PEPPOLBIS-Upgrade-Schematron.zip
+zip -r $PROJECT/target/site/files/PEPPOLBIS-Upgrade-Schematron.zip target/schematron
 
 # Example files
-zip -r target/site/files/PEPPOLBIS-Examples.zip rules/examples 
+rm -rf $PROJECT/target/site/files/PEPPOLBIS-Examples.zip
+zip -r $PROJECT/target/site/files/PEPPOLBIS-Examples.zip $PROJECT/rules/examples 
 
 # Guides
 docker run --rm -i -v $PROJECT:/documents -v $PROJECT/target:/target difi/asciidoctor
