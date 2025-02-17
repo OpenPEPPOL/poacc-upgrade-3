0538e29def7783fb9df55b8bc86c207b8f3c3a76
Hans Petersen <h.petersen@nleyes.com>
2025-02-17 15:51:38 +0100

    initial commit

--- build.bat
+++ build.bat
@@ -21,12 +21,29 @@ for %%s in ("%PROJECT%\rules\sch\*.sch") do (
     docker run --rm -i -v "%PROJECT%:/src" -v "%PROJECT%\target\schematron:/target" klakegg/schematron prepare /src/rules/sch/%%~nxs /target/%%~nxs
 )
 
-docker run --rm -i -v "%PROJECT%\target\site\files:/src" alpine:3.6 rm -rf /src/PEPPOLBIS-Upgrade-Schematron.zip
-docker run --rm -i -v "%PROJECT%\target\schematron:/src" -v "%PROJECT%\target\site\files:/target" -w /src kramos/alpine-zip -r /target/PEPPOLBIS-Upgrade-Schematron.zip .
+set "zipname=PEPPOLBIS-Upgrade-Schematron.zip"
+set zip="%PROJECT%\target\site\files\PEPPOLBIS-Upgrade-Schematron.zip"
+if exist %zip% (
+    echo remove %zip%
+    del %zip%
+)
+
+powershell -command "Compress-Archive -Path '%PROJECT%\target\schematron\*' -DestinationPath '%zip%' -Force"
+
+::docker run --rm -i -v "%PROJECT%\target\site\files:/src" alpine:3.6 rm -rf /src/PEPPOLBIS-Upgrade-Schematron.zip
+::docker run --rm -i -v "%PROJECT%\target\schematron:/src" -v "%PROJECT%\target\site\files:/target" -w /src kramos/alpine-zip -r /target/PEPPOLBIS-Upgrade-Schematron.zip .
 
 :: Example files
-docker run --rm -i -v "%PROJECT%\target\site\files:/src" alpine:3.6 rm -rf /src/PEPPOLBIS-Examples.zip
-docker run --rm -i -v "%PROJECT%\rules\examples:/src" -v "%PROJECT%\target\site\files:/target" -w /src kramos/alpine-zip -r /target/PEPPOLBIS-Examples.zip .
+set "zipname=PEPPOLBIS-Examples.zip"
+set "zip=%PROJECT%\target\site\files\%zipname%"
+if exist %zip% (
+    echo remove %zip%
+    del %zip%
+)
+
+powershell -command "Compress-Archive -Path '%PROJECT%/rules/examples/*' -DestinationPath '%zip%' -Force"
+::docker run --rm -i -v "%PROJECT%\target\site\files:/src" alpine:3.6 rm -rf /src/PEPPOLBIS-Examples.zip
+::docker run --rm -i -v "%PROJECT%\rules\examples:/src" -v "%PROJECT%\target\site\files:/target" -w /src kramos/alpine-zip -r /target/PEPPOLBIS-Examples.zip .
 
 :: Guides
 docker run --rm -i -v "%PROJECT%:/documents" -v "%PROJECT%\target:/target" difi/asciidoctor
@@ -35,4 +52,4 @@ docker run --rm -i -v "%PROJECT%:/documents" -v "%PROJECT%\target:/target" difi/
 :: You can skip this part or just leave it for Unix-like environments using Docker.
 
 endlocal
-pause
+
